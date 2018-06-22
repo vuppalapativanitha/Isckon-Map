a620e0cb3e5893630fd10d94913859b98daf822c
vuppalapativanitha <40449905+vuppalapativanitha@users.noreply.github.com>
2018-06-22 02:55:24 -0700

    Add files via upload

---
+++ my map/js/data.js
@@ -0,0 +1,76 @@
+/* Model Data */
+
+var locations = [
+
+    {
+        title: 'Isckon Temple - Banglore',
+        location: {
+            lat: 12.8929, 
+            lng: 77.5491
+        }
+    },
+    {
+        title: 'Isckon Temple - Vrindavan',
+        location: {
+            lat: 27.5723,
+            lng: 77.6774
+        }
+    },
+    {
+        title: 'Isckon Temple - Mayapur',
+        location: {
+            lat: 23.4251,
+            lng: 88.3902
+        }
+    },
+   
+    {
+        title: 'Isckon Temple - Delhi',
+        location: {
+            lat: 28.5558,
+            lng: 77.2543 
+        }
+    },
+     {
+        title: 'Isckon Temple - Mumbai',
+        location: {
+            lat: 19.1130,
+            lng: 72.8266
+        }
+    },
+     {
+        title: 'Isckon Temple - Pune',
+        location: {
+            lat: 18.5201,
+            lng: 73.8793 
+        }
+    },
+     {
+        title: 'Isckon Temple - Hyderabad',
+        location: {
+            lat: 17.3880,
+            lng: 78.4748
+        }
+    },
+    {
+        title: 'Isckon Temple - Noida',
+        location: {
+            lat: 28.5866,
+            lng: 77.3505
+        }
+    },
+    {
+        title: 'Isckon Temple - Ahmedabad',
+        location: {
+            lat: 23.0284,
+            lng: 72.5068
+        }
+    },
+        {
+        title: 'Isckon Temple - Kanpur',
+        location: {
+            lat: 26.5269,
+            lng: 80.2779
+        }
+    }
+];
